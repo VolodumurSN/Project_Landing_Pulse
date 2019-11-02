30123ae3cf23513176dcb6b9f62e587977784785
Vova <vova77gh@gmail.com>
2019-11-02 08:55:44 +0200

    add slick slider

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){ //download document with JQuery and run code when it ready
+    $('.carousel__inner').slick({
+        speed: 1200,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/slider_prev.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/slider_next.svg"></button>',
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                dots: true,
+                arrows:false
+              }
+            }
+          ]
+        
+    });
+  });
