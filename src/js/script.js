5202e7e1afdd9a09229ab041d729534a895f0407
Vova <vova77gh@gmail.com>
2019-11-08 06:56:54 +0200

    adaptive design and catalog card

--- src/js/script.js
+++ src/js/script.js
@@ -3,14 +3,14 @@ $(document).ready(function(){ //download document with JQuery and run code when 
         speed: 1200,
         autoplay: true,
         autoplaySpeed: 4000,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/slider_prev.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/slider_next.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider_prev.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/slider_next.svg"></button>',
         responsive: [
             {
               breakpoint: 992,
               settings: {
-                dots: true,
-                arrows:false
+                arrows:false,
+                respondTo: '.carousel__inner'
               }
             }
           ]
